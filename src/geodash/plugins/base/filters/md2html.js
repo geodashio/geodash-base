3a70c3b3a5d83bb62b848fd3cf63cac2c3971f5d
pjdufour <pjdufour.dev@gmail.com>
2016-07-13 17:06:20 -0700

    markdown renderer enhancements

--- src/geodash/plugins/base/filters/md2html.js
+++ src/geodash/plugins/base/filters/md2html.js
@@ -7,12 +7,21 @@ geodash.filters["md2html"] = function()
       var converter = new showdown.Converter();
       html = converter.makeHtml(text);
       // Remove Prefix/Suffix Paragraph Tags
-      html = html.substring("<p>".length, html.length - "</p>".length);
+      
+      //html = html.substring("<p>".length, html.length - "</p>".length);
+
       // Open Links in New Windows
-      var pattern = new RegExp("(<a .*)>(.*?)</a>", "gi");
-      html = html.replace(pattern, '$1 target="_blank">$2</a>');
+      html = html.replace(new RegExp("(<a .*?)>(.*?)</a>", "gi"), '$1 target="_blank">$2</a>');
+
       // Replace New Line characters with Line Breaks
       html = html.replace(new RegExp('\n', 'gi'),'<br>');
+
+      // Replace extra new lines before heading tags, which add their own margin by default
+      html = html.replace(new RegExp("<br><br><(h\\d.*?)>", "gi"),'<br><$1>');
+
+      // Replace extra new lines before paragraph tags, which add their own margin by default
+      html = html.replace(new RegExp("<br><br><p>", "gi"),'<p>');
+
       return html;
     }
     else
